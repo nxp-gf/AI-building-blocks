799d397b4e9017c778ac33e5fa3904a5182fd38c
gf <you@example.com>
2018-12-24 17:30:58 +0000

    traning server updated

--- train/train.py
+++ train/train.py
@@ -1,13 +1,5 @@
-from tensorflow.keras.preprocessing import image
 import os
-from autokeras.image.image_supervised import load_image_dataset, ImageClassifier
-from keras.models import load_model
-from keras.utils import plot_model
-from keras.preprocessing.image import load_img, img_to_array
-import numpy as np
-from keras.models import load_model
-from keras.layers import Activation
-from keras.preprocessing.image import ImageDataGenerator
+import multiprocessing
 
 TEST_IMG_DIR_INPUT = "/root/re/test"
 TEST_IMG_DIR_OUTPUT = "/root/re/test-formatted"
@@ -22,15 +14,25 @@ MODEL_DIR = '/root/re/my_model.h4'
 trn_path = '/root/re/train-ge'
 val_path = '/root/re/test-ge'
 
+def train(filename):
+    from tensorflow.keras.preprocessing import image
+    from autokeras.image.image_supervised import load_image_dataset, ImageClassifier
+    from keras.models import load_model
+    from keras.utils import plot_model
+    from keras.preprocessing.image import load_img, img_to_array
+    from keras.models import load_model
+    from keras.layers import Activation
+    from keras.preprocessing.image import ImageDataGenerator
+    import numpy as np
 
-def format_img(input_dir, output_dir):
-    for file_name in os.listdir(input_dir):
-        path_name = os.path.join(input_dir, file_name)
-        img = image.load_img(path_name, target_size=(IMAGE_SIZE, IMAGE_SIZE))
-        path_name = os.path.join(output_dir, file_name)
-        img.save(path_name)
+    def format_img(input_dir, output_dir):
+        for file_name in os.listdir(input_dir):
+            path_name = os.path.join(input_dir, file_name)
+            img = image.load_img(path_name, target_size=(IMAGE_SIZE, IMAGE_SIZE))
+            path_name = os.path.join(output_dir, file_name)
+            img.save(path_name)
 
-if __name__ == '__main__':
+    print("Training started")
     format_img(TEST_IMG_DIR_INPUT, TEST_IMG_DIR_OUTPUT)
     format_img(TRAIN_IMG_DIR_INPUT, TRAIN_IMG_DIR_OUTPUT)
     train_data, train_labels = load_image_dataset(csv_file_path=TRAIN_CSV_DIR, images_path=TRAIN_IMG_DIR)
@@ -54,5 +56,54 @@ if __name__ == '__main__':
     trn_data = generator.flow_from_directory(trn_path, batch_size=32, target_size=(28, 28))
     val_data = generator.flow_from_directory(val_path, batch_size=32, target_size=(28, 28))
     automodel.fit_generator(trn_data,200,10)
-    automodel.save('/root/building-blocks/finalmodel')
+    automodel.save(filename)
+    print("Training ended")
 
+def kafka_handler():
+    from kafka import KafkaConsumer
+    from kafka import KafkaProducer
+
+    topic_updated = "NXP_MODELS_UPDATED"
+    topic_training = "NXP_DATASET_TRAINING"
+    consumer = KafkaConsumer(bootstrap_servers='10.193.20.98:9092')
+    producer = KafkaProducer(bootstrap_servers='10.193.20.98:9092')
+    consumer.subscribe([topic_training])
+    for msg in consumer:
+        filename = "/root/building-blocks/finalmodel"
+        train(filename)
+        future = producer.send(topic_updated, filename.encode())
+        result = future.get(timeout=60)
+
+train_process = multiprocessing.Process(target = kafka_handler, args = ())
+train_process.start()
+
+from flask import Flask, send_from_directory
+from flask_restful import reqparse, abort, Api, Resource, request
+
+app = Flask(__name__)
+api = Api(app)
+
+class TrainModels(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type=str, location='args')
+        self.modelspath = "./models"
+    def get(self):
+        return send_from_directory(self.modelspath, data.name, as_attachment=True)
+
+    def delete(self):
+        return {'state':'SUCCESS'}, 200
+
+    def put(self):
+        return {'state':'SUCCESS'}, 200
+
+    def post(self):
+        return {'state':'SUCCESS'}, 200
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TrainModels, '/models')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050, threaded=True)
